be402a81be7d3834811908c6da3c6ce3eea8478e
displayflex <3tonishere@gmail.com>
2018-10-05 18:57:25 +0300

    первые шаги

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+	
+	$list = scandir('data');
+	
+	foreach($list as $fname){
+		if(is_file("data/$fname")){
+			echo "<a href=\"post.php?fname=$fname\">$fname</a><br>";
+		}
+	}
+	
+?>
+<a href="add.php">Добавить</a>
